6f3b3ef51aacd17e024a3d22b78f983f0c45b86a
Gabriel Chen <gabriel.luke.chen@gmail.com>
2020-04-04 01:38:40 -0700

    cors mode in fetch api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,5 +4,19 @@ import { RSS_URL } from 'helpers/config';
 const PARSER = new Parser();
 
 export function fetchRss() {
-  return PARSER.parseURL(RSS_URL);
+  return fetch(RSS_URL, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      accept: 'application/rss+xml',
+    },
+  }).then((response) => response.text())
+    // .then((response) => response.json())
+    .then((data) => {
+      if (!data) {
+        throw new Error('RSS request failed');
+      }
+      return PARSER.parseString(data);
+    });
 }
